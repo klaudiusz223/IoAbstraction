54f4d46676d98d06e43dab558674a800c76bbc89
klaudiuszak <klaudiuszak@gmail.com>
2018-12-11 13:12:56 +0100

    Allowing MAX_KEYS to be set during build

--- src/SwitchInput.h
+++ src/SwitchInput.h
@@ -28,7 +28,9 @@
  * If you want more (or less) buttons, change this definition below to the appropriate number.
  * Each button adds about 6 bytes of RAM, so on a tiny you could adjust downwards for example.
  */
+#ifndef MAX_KEYS
 #define MAX_KEYS 4
+#endif
 
 // END user adjustable section
 
